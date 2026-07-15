d6197a5874dab06392d63a579eb54c1fd0df2efc
Tvisha Raj <tvisharaj2@gmail.com>
2026-07-14 20:05:30 -0700

    added dropdown

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("Safe to Swim")
 st.write("Is it safe to swim today?")
+lake= st.selectbox("Select your lake", ["Select from dropdown menu", "Sammamish Lake", "Pine Lake"])
+check=st.button("Check out your lake")
+if check:
+    st.write(lake)
